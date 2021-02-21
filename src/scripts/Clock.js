a156ef97c7e5df82ed2ed2d832291b033583bbc3
QuentinBALCEREK <quentin.balcerek@gmail.com>
2021-02-21 01:52:18 +0100

    docs(clock): function of class clock

--- src/scripts/Clock.js
+++ src/scripts/Clock.js
@@ -6,37 +6,57 @@ var Clock = function () {
 	this.reste;
 	this.arrondi;
 
+	/**
+	 * Run all functions
+	 */
 	this.run = function () {
 		this.init();
 		this.resetClock();
 		this.setClockElement("heure");
 		this.displayHour();
-		this.displayMinute();
-		this.setRest();
-		this.displaySeconde();
+		this.displayTenMinuteDigit();
+		this.displayUniteMinuteDigit();
+		this.displaySecond();
 	}
 
+	/**
+	 * Initiate all variables
+	 */
 	this.init = function () {
 		var date = new Date();
 		this.date = date;
 		this.heure = date.getHours();
 		this.minutes = date.getMinutes();
-		this.seconde = date.getSeconds();
+		this.second = date.getSeconds();
 		this.reste = 0;
 		this.arrondi = Math.floor(this.minutes / 5);
 	}
 
-	this.setOpacity = function (id, value) {
+	/**
+	 * Set element opcity
+	 * @param {string} id html id of the element
+	 * @param {int} opacity value of opacity
+	 */
+	this.setOpacity = function (id, opacity) {
 		var element = document.getElementById(id);
-		element.style.opacity = value / 100;
-		element.style.filter = 'alpha(opacity=' + value + ')';
+		element.style.opacity = opacity / 100;
+		element.style.filter = 'alpha(opacity=' + opacity + ')';
 	}
 
-	this.setOpacityLoop = function (length, element, opacity) {
+	/**
+	 * Loop for Set element opcity
+	 * @param {int} length lenght of the loop
+	 * @param {string} id html id of the element
+	 * @param {int} opacity value of opacity
+	 */
+	this.setOpacityLoop = function (length, id, opacity) {
 		for (var i = 1; i <= length; i++)
-			this.setOpacity(element + i, opacity);
+			this.setOpacity(id + i, opacity);
 	}
 
+	/**
+	 * Set empty clock
+	 */
 	this.resetClock = function () {
 		this.setOpacityLoop(12, "heure_", 20);
 		this.setOpacityLoop(11, "minutes_", 20);
@@ -44,12 +64,19 @@ var Clock = function () {
 		this.setOpacityLoop(60, "seconde_", 20);
 	}
 
-	this.setClockElement = function ($idElem) {
+	/**
+	 * Display the clock
+	 * @param {string} id html id of the element
+	 */
+	this.setClockElement = function (id) {
 		document
-			.getElementById($idElem)
-			.innerHTML = 'Heure : ' + this.heure + ' : ' + this.minutes + ' : ' + this.seconde;
+			.getElementById(id)
+			.innerHTML = 'Heure : ' + this.heure + ' : ' + this.minutes + ' : ' + this.second;
 	}
 
+	/**
+	 * Display hours
+	 */
 	this.displayHour = function () {
 		if (this.heure > 12)
 			this.heure = this.heure - 12;
@@ -60,7 +87,10 @@ var Clock = function () {
 		this.setOpacity("heure_" + this.heure, 100);
 	}
 
-	this.displayMinute = function () {
+	/**
+	* Display ten-minutes digit
+	*/
+	this.displayTenMinuteDigit = function () {
 		if (this.arrondi > 0) {
 			this.setOpacity("minutes_" + this.arrondi, 100);
 			this.reste = this.minutes - this.arrondi * 5;
@@ -72,13 +102,19 @@ var Clock = function () {
 		return true;
 	}
 
-	this.displaySeconde = function () {
-		this.setOpacityLoop(this.seconde, "seconde_", 100);
-	}
-
-	this.setRest = function () {
+	/**
+	* Display unit minute digit
+	*/
+	this.displayUniteMinuteDigit = function () {
 		if (this.reste > 0) {
 			this.setOpacityLoop(this.reste, "minute_", 100);
 		}
+	}
+
+	/**
+	* Display seconds
+	*/
+	this.displaySecond = function () {
+		this.setOpacityLoop(this.second, "seconde_", 100);
 	}
 }
