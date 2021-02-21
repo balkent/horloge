6c8ac9beb307f25ff5a7063bb6979571b1a95c12
QuentinBALCEREK <quentin.balcerek@gmail.com>
2021-02-21 12:28:52 +0100

    build(core): new structure of project

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
+project_path    = "src"
 http_path       = "/"
-css_dir         = "stylesheets"
+css_dir         = "css"
 sass_dir        = "sass"
-images_dir      = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
