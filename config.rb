a8c065c5bdf9a8e331221188f0a3bb7374fc7011
QuentinBALCEREK <quentin.balcerek@gmail.com>
2021-02-21 12:45:24 +0100

    build(scss): compact output

--- config.rb
+++ config.rb
@@ -9,7 +9,8 @@ sass_dir        = "sass"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+# :nested, :expanded, :compact, or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
